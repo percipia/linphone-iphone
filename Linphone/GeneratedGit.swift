dd8b3915e90cdf3f48a59e0ea4161fa70fc9ccaa
mkravos <44103767+mkravos@users.noreply.github.com>
2026-01-08 11:09:49 -0600

    bumped marketing version

--- Linphone/GeneratedGit.swift
+++ Linphone/GeneratedGit.swift
@@ -3,5 +3,5 @@ import Foundation
 public enum AppGitInfo {
     public static let branch = "percipia"
     public static let commit = "1d8842917"
-    public static let tag = "6.1.0-alpha"
+    public static let tag = "6.1.1-alpha"
 }
