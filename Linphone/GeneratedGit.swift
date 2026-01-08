330250e75718a1fe1533fb9341eb619ce1ca7cb8
mkravos <44103767+mkravos@users.noreply.github.com>
2026-01-08 15:27:24 -0600

    moved AppIntentVocabulary to main app root

--- Linphone/GeneratedGit.swift
+++ Linphone/GeneratedGit.swift
@@ -2,6 +2,6 @@ import Foundation
 
 public enum AppGitInfo {
     public static let branch = "percipia"
-    public static let commit = "1d8842917"
-    public static let tag = "6.1.1-alpha"
+    public static let commit = "50acb22c8"
+    public static let tag = "6.1.0-alpha"
 }
