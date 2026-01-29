087d7ebe0f2cce0903ac2b80da8bd4625f601eb3
mkravos <44103767+mkravos@users.noreply.github.com>
2026-01-29 15:15:05 -0600

    version bump

--- Linphone/GeneratedGit.swift
+++ Linphone/GeneratedGit.swift
@@ -3,5 +3,5 @@ import Foundation
 public enum AppGitInfo {
     public static let branch = "percipia"
     public static let commit = "6d364a873"
-    public static let tag = "6.1.0-alpha"
+    public static let tag = "6.1.3-alpha"
 }
