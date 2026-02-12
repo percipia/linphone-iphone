69f8919ee30f719e184ba0461efb0ad4a9e7ca97
mkravos <44103767+mkravos@users.noreply.github.com>
2026-02-12 15:34:36 -0600

    version tag updated

--- Linphone/GeneratedGit.swift
+++ Linphone/GeneratedGit.swift
@@ -2,6 +2,6 @@ import Foundation
 
 public enum AppGitInfo {
     public static let branch = "percipia"
-    public static let commit = "61ed9690c"
-    public static let tag = "6.1.0-alpha"
+    public static let commit = "319b6e4d4"
+    public static let tag = "2.1.0"
 }
