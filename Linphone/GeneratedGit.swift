13aae59694e6626fe8bbd74fb43410bb59788541
mkravos <44103767+mkravos@users.noreply.github.com>
2026-03-17 11:46:02 -0500

    updated version code and build numbers

--- Linphone/GeneratedGit.swift
+++ Linphone/GeneratedGit.swift
@@ -3,5 +3,5 @@ import Foundation
 public enum AppGitInfo {
     public static let branch = "percipia"
     public static let commit = "159877b10"
-    public static let tag = "2.2.1"
+    public static let tag = "2.2.2"
 }
