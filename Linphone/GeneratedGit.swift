167aa81c96a944e21928c86f93905a8d76530751
Benoit Martins <benoit.martins@belledonne-communications.com>
2026-06-22 18:26:10 +0200

    Allow linphone-sdk-swift-ios updates up to next major version

--- Linphone/GeneratedGit.swift
+++ Linphone/GeneratedGit.swift
@@ -2,6 +2,6 @@ import Foundation
 
 public enum AppGitInfo {
     public static let branch = "master"
-    public static let commit = "44bb74efc"
+    public static let commit = "242a12738"
     public static let tag = "6.3.0-alpha"
 }
