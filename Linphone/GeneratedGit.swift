ad805cd45c415a5bd517b1729072dab39fc80697
Benoit Martins <benoit.martins@belledonne-communications.com>
2026-06-22 18:29:37 +0200

    Fix remote video not displayed after video activation

--- Linphone/GeneratedGit.swift
+++ Linphone/GeneratedGit.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public enum AppGitInfo {
-    public static let branch = "master"
-    public static let commit = "242a12738"
+    public static let branch = "fix/remote_video"
+    public static let commit = "adcfd1e3f"
     public static let tag = "6.3.0-alpha"
 }
